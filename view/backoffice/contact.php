f17705d9c2dc589784bea00032b964db98aefaab
Dina <dina.m.hamdane@gmail.com>
2024-09-10 17:34:06 +0200

    modified css for backoffice contact message.

--- view/backoffice/contact.php
+++ view/backoffice/contact.php
@@ -6,13 +6,19 @@
 
             <?php foreach ($args['session']['listContactMessage'] as $args['session']['contactMessage']) { ?>
                 <div class="backoffice-option-container">
-                    <h2> Type de message : <?= $args['session']['contactMessage']['type'] ?> </h2>
+                    <h4> Message de : <?= $args['session']['contactMessage']['firstName'] ?> <?= $args['session']['contactMessage']['lastName'] ?></h4>
+                    <p>Email <a href="mailto:<?= $args['session']['contactMessage']['email'] ?> "><?= $args['session']['contactMessage']['email'] ?></a>
+                    </p>
+                    <p>Téléphone : <a href="tel:<?= $args['session']['contactMessage']['phoneNumber'] ?>"><?= $args['session']['contactMessage']['phoneNumber'] ?></a>
+                    </p>
+                    <p> Type de message : <?= $args['session']['contactMessage']['type'] ?></p>
+                    <div class="contact-message">
+                        <p> Message : <?= $args['session']['contactMessage']['content'] ?></p>
+                    </div>
                     <p> Le : <?= $args['session']['contactMessage']['time'] ?></p>
-                    <p> Par : <?= $args['session']['contactMessage']['firstName'] ?> <?= $args['session']['contactMessage']['lastName'] ?></p>
-                    <p> Email: <?= $args['session']['contactMessage']['email'] ?></p>
-                    <p>Téléphone : <?= $args['session']['contactMessage']['phoneNumber'] ?></p>
-                    <p> Message : <?= $args['session']['contactMessage']['content'] ?></p>
-                    <a href="/ctrl/backoffice/contactMessage-detail.php?id=<?= ($args['session']['contactMessage']['id']) ?>">Voir Détails</a>
+
+                    <!--                     <a href="/ctrl/backoffice/contactMessage-detail.php?id=<?= ($args['session']['contactMessage']['id']) ?>">Voir Détails</a>
+ -->
                 </div>
             <?php } ?>
 
