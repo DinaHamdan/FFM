c0760efbeb8b6e29875ac600e65c81c0daefac8b
Dina <dina.m.hamdane@gmail.com>
2024-09-11 18:21:58 +0200

    added css for backoffice that lists members.

--- view/backoffice/list-members.php
+++ view/backoffice/list-members.php
@@ -2,17 +2,18 @@
     <section class="backoffice-option" id="members">
         <h1 id="page-title"><?= $args['pageTitle'] ?></h1>
 
-        <div>
+        <div id="backoffice-member">
 
 
             <?php foreach ($args['session']['listMember'] as $args['session']['member']) { ?>
                 <div class="backoffice-option-container" id="member-container">
-                    <p> Prénom: <?= $args['session']['member']['firstName'] ?> Nom :<?= $args['session']['member']['lastName'] ?></p>
+                    <p> Prénom: <?= $args['session']['member']['firstName'] ?> </p>
+                    <p> Nom :<?= $args['session']['member']['lastName'] ?></p>
                     <p> Email: <?= $args['session']['member']['email'] ?></p>
                     <p>Téléphone : <?= $args['session']['member']['phoneNumber'] ?></p>
                     <img id="profile-photo" src="data:image/png;base64,<?= base64_encode($args['session']['member']['avatar']) ?>" alt="member-Avatar">
                     <?php if ($args['session']['user']['codeRole'] == 'ADM') { ?>
-                        <p> <a href="/ctrl/backoffice/delete-member.php?id=<?= $args['session']['member']['id'] ?>">Remove</a></p>
+                        <p> <a href="/ctrl/backoffice/delete-member.php?id=<?= $args['session']['member']['id'] ?>">Enlever</a></p>
 
                     <?php  } ?>
                 </div>
